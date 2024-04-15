f6b24e1510186c96bd63ecda09b21c95e7615719
Ujjwal <ujjwalkamila86@gmail.com>
2024-04-15 23:05:27 +0530

    palindrone string without ussing iteration

---
+++ @Python Lab Codes/Python Exam preparation/check_palindrome.py
@@ -0,0 +1,12 @@
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    return s[0] == s[-1] and is_palindrome(s[1:-1])
+
+# Test the function
+input_string = input("Enter a string: ")
+
+if is_palindrome(input_string):
+    print("The given string is a palindrome.")
+else:
+    print("The given string is not a palindrome.")

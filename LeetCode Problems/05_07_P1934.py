a0fa14bba9f4165f1c19badfed733c413ca1523b
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-07-06 00:29:48 +0530

    Find Lucky Integer in an Array

---
+++ LeetCode Problems/05_07_P1934.py
@@ -0,0 +1,26 @@
+# 1394. Find Lucky Integer in an Array
+
+from collections import Counter
+class Solution:
+    def findLucky(self, arr):
+        count = Counter(arr)
+        result = -1
+
+        for i in range(1, 501):  
+            if count[i] == i:
+                if i > result:
+                    result = i
+
+        return result
+
+
+# test cases
+sol = Solution()
+res = sol.findLucky([2,2,3,4])
+print("Output is :",res)
+
+sol = Solution()
+res = sol.findLucky([1,2,2,3,3,3])
+print("Output is :",res)
+res = sol.findLucky([2,2,2,3,3])
+print("Output is :",res)

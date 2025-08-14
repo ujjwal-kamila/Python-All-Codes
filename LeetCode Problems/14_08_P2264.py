a89f2a2e259a6a22c8747db950612083e4b53df2
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-14 23:43:06 +0530

    Largest 3-Same-Digit Number in String

--- LeetCode Problems/14_08_P2264.py
+++ LeetCode Problems/14_08_P2264.py
@@ -18,7 +18,7 @@ class Solution:
         ans = ""
         for i in range(len(num)-2):
             if num[i] ==num[i+1] == num[i+2]:
-                ans = max(ans, num[i:i+2])
+                ans = max(ans, num[i:i+3])
         return ans 
     
 print(Solution().largestGoodInteger("6777133339"))  

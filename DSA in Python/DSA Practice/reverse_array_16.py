d7d074721f499e3b16018464c0dafa0a762f2244
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-15 21:21:18 +0530

    Reverse an Array Using Recursion  and loops

--- DSA in Python/DSA Practice/reverse_array_16.py
+++ DSA in Python/DSA Practice/reverse_array_16.py
@@ -41,3 +41,14 @@ def reverse_array(arr):
 # Example
 arr = [1, 2, 3, 4, 5]
 print(reverse_array(arr))
+
+
+# reverse using while loop 
+arr = [1,2,3,4,5]
+left = 0
+right = len(arr)-1
+while left < right:
+    arr[left],arr[right]=arr[right],arr[left]
+    left+=1
+    right=-1
+print(arr)

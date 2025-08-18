9696e19dd8e89d5db281bc143482e5b282f81003
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-18 23:12:57 +0530

    Leetcode : 509. Fibonacci Number

---
+++ DSA in Python/DSA Practice/fibonacchi_num18.py
@@ -0,0 +1,25 @@
+# Find the Fibonacci Number
+# Leetcode : 509. Fibonacci Number
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n ==1 :
+            return n
+        return self.fib(n-1)+self.fib(n-2)
+
+ans = Solution().fib(5)
+print(ans)
+
+# or can do using 
+
+class Solution:
+    def fun(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        return self.fun(n-1) + self.fun(n-2)
+
+    def fib(self, n: int) -> int:
+        return self.fun(n)
+
+ 
+print(Solution().fib(5))  

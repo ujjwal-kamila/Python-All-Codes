138e9e64f24c557c11ad451940dea50aea78571e
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-20 23:16:05 +0530

    Leetcode Problem : 7.Reverse Integer

--- LeetCode Problems/20_08_7.py
+++ LeetCode Problems/20_08_7.py
@@ -2,6 +2,7 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
+        int_min, int_max = -2**31, 2**31 - 1
         neg = x < 0
         x = abs(x)
         str_x = str(x)
@@ -10,16 +11,21 @@ class Solution:
         rev_num = int(str_x[::-1])
         if neg:
             rev_num = -rev_num
+        # for contraints 
+        if rev_num < int_min or rev_num > int_max:
+            return 0
         return rev_num
 
 ans  = Solution().reverse(-123456)
+ans  = Solution().reverse(1534236469)
 print(ans)
 
 
-''' Another method '''
+''' Another method : not optimised'''
 
 class Solution:
     def reverse(self, x: int) -> int:
+        int_min, int_max = -2**31, 2**31 - 1
         neg = x < 0
         x = abs(x)
         rev_num = 0 
@@ -30,9 +36,14 @@ class Solution:
             x//=10
         if neg:
             return -rev_num
+        # for contraints 
+        if rev_num < int_min or rev_num > int_max:
+            return 0
         return rev_num
         
                 
 
 ans  = Solution().reverse(-123456)
+ans  = Solution().reverse(1534236469)
+
 print(ans)

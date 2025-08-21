4f7c5c6430a35d1b4f5ef5c528c2bdd77d758923
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-22 01:15:32 +0530

    Bubble Sort in Python

--- DSA in Python/DSA Practice/bubble_sort20.py
+++ DSA in Python/DSA Practice/bubble_sort20.py
@@ -1,2 +1,20 @@
 #  Bubble Sort in Python 
 
+# Assending order sorting 
+class Solution:
+    def bubble_sort(self,arr):
+        n = len(arr)
+        for i in range(n-2,-1,-1):
+            for j in range(0,i+1):
+                if arr[j]>arr[j+1]:
+                    arr[j],arr[j+1] = arr[j+1],arr[j]
+        return arr
+
+
+# Testing
+arr = [5, 8, 1, 6, 9, 2, 4]
+print("Original : ", arr)
+
+ans =Solution().bubble_sort(arr)
+
+print("Sorted : ", ans)

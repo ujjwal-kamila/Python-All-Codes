8ab9554fd7c7e162a609e708d9de5c60cc8f034e
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-08-22 01:22:23 +0530

    Best case updated : Bubble Sort in Python

--- DSA in Python/DSA Practice/bubble_sort20.py
+++ DSA in Python/DSA Practice/bubble_sort20.py
@@ -18,3 +18,22 @@ print("Original : ", arr)
 ans =Solution().bubble_sort(arr)
 
 print("Sorted : ", ans)
+
+
+
+
+
+
+# "Best case "
+# class Solution:
+#     def bubble_sort(self, arr):
+#         n = len(arr)
+#         for i in range(n-1):
+#             swapped = False       
+#             for j in range(0, n-i-1):   
+#                 if arr[j] > arr[j+1]:
+#                     arr[j], arr[j+1] = arr[j+1], arr[j]
+#                     swapped = True
+#             if not swapped:        # if no swap-> array is sorted
+#                 break
+#         return arr
